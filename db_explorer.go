2cc993042bea5cedb3627a47889e721c918e6ce5
ai.pchelkina <ai.pchelkina@gmail.com>
2021-06-03 11:37:05 +0300

    закончила deletePage

--- db_explorer.go
+++ db_explorer.go
@@ -42,70 +42,94 @@ type requestParametrs struct {
 	limit  int
 }
 
+type preparedQuery struct {
+	value    *string
+	required bool
+}
+
 type response map[string]interface{}
 
-func prepareQueryValue(qp *queryParametrs, column string, value interface{}, isInsert bool) (*string, error) {
+func prepareQueryValue(columnInfo columnTable, reqValues map[string]interface{}, isInsert bool) (preparedQuery, error) {
 	var preparedString string
+	var columnExists bool
+	var reqValue interface{}
 
-	tableColumns, ok := qp.tablesFromBD[*qp.queryRequestParametrs.table]
-	if !ok {
-		return nil, fmt.Errorf("query table is nil")
-	}
-
-	for _, col := range tableColumns {
-		if col.fieldCol == column {
-			if value == nil {
-				if col.nullCol == "YES" {
-					preparedString = "NULL"
-					return &preparedString, nil
-				} else {
-					return nil, fmt.Errorf("field %v have invalid type", col.fieldCol)
-				}
-			}
-
-			if col.keyCol.Valid {
-				if col.keyCol.String == "PRI" {
-					if isInsert {
-						preparedString = ""
-						return &preparedString, nil
-					} else {
-						return nil, fmt.Errorf("field %v have invalid type", col.fieldCol)
-					}
-				}
-			}
-
-			var typeOk bool
-
-			switch value.(type) {
-			case int:
-				if col.typeCol == "int" {
-					typeOk = true
-					preparedString = strconv.Itoa(value.(int))
-				}
-			case float64:
-				if col.typeCol == "int" {
-					typeOk = true
-					preparedString = strconv.FormatFloat(value.(float64), 'f', -1, 64)
-				}
-			case string:
-				if col.typeCol == "varchar(255)" || col.typeCol == "text" {
-					typeOk = true
-					preparedString = "'" + value.(string) + "'"
-				}
-			}
-
-			if !typeOk {
-				return nil, fmt.Errorf("field %v have invalid type", col.fieldCol)
-			}
-
+	for column, value := range reqValues {
+		if column == columnInfo.fieldCol {
+			columnExists = true
+			reqValue = value
 			break
 		}
 	}
 
-	return &preparedString, nil
+	if columnExists {
+		if reqValue == nil {
+			if columnInfo.nullCol == "YES" {
+				return preparedQuery{nil, true}, nil
+			} else {
+				return preparedQuery{nil, false},
+					fmt.Errorf("field %v have invalid type", columnInfo.fieldCol)
+			}
+		}
+
+		if columnInfo.keyCol.Valid {
+			if columnInfo.keyCol.String == "PRI" {
+				if isInsert {
+					return preparedQuery{nil, false}, nil
+				} else {
+					return preparedQuery{nil, false},
+						fmt.Errorf("field %v have invalid type", columnInfo.fieldCol)
+				}
+			}
+		}
+
+		var typeOk bool
+
+		switch reqValue.(type) {
+		case int:
+			if columnInfo.typeCol == "int" {
+				typeOk = true
+				preparedString = strconv.Itoa(reqValue.(int))
+			}
+		case float64:
+			if columnInfo.typeCol == "int" {
+				typeOk = true
+				preparedString = strconv.FormatFloat(reqValue.(float64), 'f', -1, 64)
+			}
+		case string:
+			if columnInfo.typeCol == "varchar(255)" || columnInfo.typeCol == "text" {
+				typeOk = true
+				preparedString = reqValue.(string)
+			}
+		}
+
+		if !typeOk {
+			return preparedQuery{nil, false},
+				fmt.Errorf("field %v have invalid type", columnInfo.fieldCol)
+		}
+
+		return preparedQuery{&preparedString, true}, nil
+	} else {
+		if isInsert {
+			if columnInfo.nullCol != "YES" {
+				switch columnInfo.typeCol {
+				case "varchar(255)":
+					preparedString = ""
+				case "text":
+					preparedString = ""
+				case "int":
+					preparedString = "0"
+				}
+
+				return preparedQuery{&preparedString, true}, nil
+			}
+		}
+
+		return preparedQuery{nil, false}, nil
+	}
 }
 
-func findPkQueryTable(tableName string, tablesBD map[string][]columnTable) string {
+func getPkQueryTable(tableName string, tablesBD map[string][]columnTable) string {
 	var pkCol string
 
 	columns, exists := tablesBD[tableName]
@@ -190,27 +214,15 @@ func getReqParams(r *http.Request) (requestParametrs, error) {
 		}
 	}
 
-	limit := r.URL.Query().Get("limit")
-	if limit != "" {
-		var err error
+	var err error
 
-		par.limit, err = strconv.Atoi(limit)
-		if err != nil {
-			return par, err
-		}
-	} else {
+	par.limit, err = strconv.Atoi(r.URL.Query().Get("limit"))
+	if err != nil {
 		par.limit = 5
 	}
 
-	offset := r.URL.Query().Get("offset")
-	if offset != "" {
-		var err error
-
-		par.offset, err = strconv.Atoi(offset)
-		if err != nil {
-			return par, err
-		}
-	} else {
+	par.offset, err = strconv.Atoi(r.URL.Query().Get("offset"))
+	if err != nil {
 		par.offset = 0
 	}
 
@@ -287,7 +299,7 @@ func selectData(qp *queryParametrs, db *sql.DB) (response, error, int) {
 		queryBuilder.WriteString("FROM " + *qp.queryRequestParametrs.table)
 
 		if qp.queryRequestParametrs.id != nil {
-			columnPk := findPkQueryTable(*qp.queryRequestParametrs.table, qp.tablesFromBD)
+			columnPk := getPkQueryTable(*qp.queryRequestParametrs.table, qp.tablesFromBD)
 			queryBuilder.WriteString(" WHERE ")
 			queryBuilder.WriteString(columnPk)
 			queryBuilder.WriteString(" = ?")
@@ -306,7 +318,6 @@ func selectData(qp *queryParametrs, db *sql.DB) (response, error, int) {
 		}
 
 		if err != nil {
-			fmt.Println(queryBuilder.String())
 			return nil, err, http.StatusInternalServerError
 		}
 
@@ -331,7 +342,7 @@ func selectData(qp *queryParametrs, db *sql.DB) (response, error, int) {
 			rowValues := make([]interface{}, len(rowsColumns))
 			rowPointers := make([]interface{}, len(rowsColumns))
 
-			for i, _ := range rowValues {
+			for i := range rowValues {
 				rowPointers[i] = &rowValues[i]
 			}
 
@@ -341,20 +352,19 @@ func selectData(qp *queryParametrs, db *sql.DB) (response, error, int) {
 			}
 
 			rowMap := make(map[string]interface{})
+
 			for i, val := range rowValues {
 				valByte, ok := val.([]byte)
 
 				var v interface{}
 
 				if ok {
-
 					switch rowsColumnTypes[i].DatabaseTypeName() {
 					case "INT":
 						v, _ = strconv.Atoi(string(valByte))
 					default:
 						v = string(valByte)
 					}
-
 				} else {
 					v = val
 				}
@@ -379,50 +389,93 @@ func selectData(qp *queryParametrs, db *sql.DB) (response, error, int) {
 	return resp, nil, http.StatusOK
 }
 
-func (qp *queryParametrs) updatePage(h *handler) http.HandlerFunc {
+func (qp *queryParametrs) deletePage(h *handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		req := make(map[string]interface{})
+		colPk := getPkQueryTable(*qp.queryRequestParametrs.table, qp.tablesFromBD)
 
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
+		var queryBuilder strings.Builder
 
-		err := json.Unmarshal(buf.Bytes(), &req)
+		queryBuilder.WriteString("DELETE FROM ")
+		queryBuilder.WriteString(*qp.queryRequestParametrs.table)
+		queryBuilder.WriteString(" WHERE ")
+		queryBuilder.WriteString(colPk + " = ?")
+
+		resultSql, err := h.DB.Exec(queryBuilder.String(), *qp.queryRequestParametrs.id)
 		if err != nil {
 			writeResponse(w, http.StatusInternalServerError, nil, err)
 			return
 		}
 
-		pkCol := findPkQueryTable(*qp.queryRequestParametrs.table, qp.tablesFromBD)
+		rows, err := resultSql.RowsAffected()
+		if err != nil {
+			writeResponse(w, http.StatusInternalServerError, nil, err)
+			return
+		}
+
+		result := response{"response": response{"deleted": rows}}
+
+		writeResponse(w, http.StatusOK, result, nil)
+	}
+}
+
+func (qp *queryParametrs) updatePage(h *handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		request := make(map[string]interface{})
+		buf := new(bytes.Buffer)
+
+		buf.ReadFrom(r.Body)
+
+		err := json.Unmarshal(buf.Bytes(), &request)
+		if err != nil {
+			writeResponse(w, http.StatusInternalServerError, nil, err)
+			return
+		}
+
+		pkCol := getPkQueryTable(*qp.queryRequestParametrs.table, qp.tablesFromBD)
 
 		var queryBuilder, columnBuilder strings.Builder
 		var i int
+		var values []interface{}
 
-		for columnName, columnValue := range req {
-			queryValue, err := prepareQueryValue(qp, columnName, columnValue, false)
+		tableColumns, ok := qp.tablesFromBD[*qp.queryRequestParametrs.table]
+		if !ok {
+			writeResponse(w, http.StatusInternalServerError, nil, fmt.Errorf("query table is nil"))
+			return
+		}
+
+		for _, col := range tableColumns {
+			queryValue, err := prepareQueryValue(col, request, false)
 			if err != nil {
 				writeResponse(w, http.StatusBadRequest, nil, err)
 				return
 			}
 
-			if i > 0 {
-				columnBuilder.WriteString(",")
+			if queryValue.required {
+				if i > 0 {
+					columnBuilder.WriteString(",")
+				}
+
+				columnBuilder.WriteString(col.fieldCol + " = ?")
+
+				if queryValue.value != nil {
+					values = append(values, *queryValue.value)
+				} else {
+					values = append(values, nil)
+				}
+
+				i++
 			}
-
-			columnBuilder.WriteString(columnName + " = " + *queryValue)
-
-			i++
 		}
 
 		queryBuilder.WriteString("UPDATE ")
 		queryBuilder.WriteString(*qp.queryRequestParametrs.table)
 		queryBuilder.WriteString(" SET ")
 		queryBuilder.WriteString(columnBuilder.String())
-		queryBuilder.WriteString(" ")
 		queryBuilder.WriteString(" WHERE " + pkCol + " = ?;")
 
-		fmt.Println(queryBuilder.String())
+		values = append(values, *qp.queryRequestParametrs.id)
 
-		resultSql, err := h.DB.Exec(queryBuilder.String(), *qp.queryRequestParametrs.id)
+		resultSql, err := h.DB.Exec(queryBuilder.String(), values...)
 		if err != nil {
 			writeResponse(w, http.StatusInternalServerError, nil, err)
 			return
@@ -443,8 +496,8 @@ func (qp *queryParametrs) updatePage(h *handler) http.HandlerFunc {
 func (qp *queryParametrs) insertPage(h *handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		req := make(map[string]interface{})
-
 		buf := new(bytes.Buffer)
+
 		buf.ReadFrom(r.Body)
 
 		err := json.Unmarshal(buf.Bytes(), &req)
@@ -455,22 +508,31 @@ func (qp *queryParametrs) insertPage(h *handler) http.HandlerFunc {
 
 		var queryBuilder, columnBuilder, valueBuilder strings.Builder
 		var i int
+		var values []interface{}
 
-		for column, value := range req {
-			insertValue, err := prepareQueryValue(qp, column, value, true)
+		tableColumns, ok := qp.tablesFromBD[*qp.queryRequestParametrs.table]
+		if !ok {
+			writeResponse(w, http.StatusInternalServerError, nil, fmt.Errorf("query table is nil"))
+			return
+		}
+
+		for _, col := range tableColumns {
+			insertValue, err := prepareQueryValue(col, req, true)
 			if err != nil {
 				writeResponse(w, http.StatusInternalServerError, nil, err)
 				return
 			}
 
-			if len(*insertValue) > 0 {
+			if insertValue.value != nil {
 				if i > 0 {
 					columnBuilder.WriteString(", ")
 					valueBuilder.WriteString(", ")
 				}
 
-				columnBuilder.WriteString(column)
-				valueBuilder.WriteString(*insertValue)
+				columnBuilder.WriteString(col.fieldCol)
+				valueBuilder.WriteString("?")
+
+				values = append(values, *insertValue.value)
 
 				i++
 			}
@@ -482,7 +544,7 @@ func (qp *queryParametrs) insertPage(h *handler) http.HandlerFunc {
 		queryBuilder.WriteString(" VALUES ")
 		queryBuilder.WriteString(" ( " + valueBuilder.String() + "); ")
 
-		resultSql, err := h.DB.Exec(queryBuilder.String())
+		resultSql, err := h.DB.Exec(queryBuilder.String(), values...)
 		if err != nil {
 			writeResponse(w, http.StatusInternalServerError, nil, err)
 			return
@@ -494,7 +556,8 @@ func (qp *queryParametrs) insertPage(h *handler) http.HandlerFunc {
 			return
 		}
 
-		result := response{"response": response{"id": lastID}}
+		idCol := getPkQueryTable(*qp.queryRequestParametrs.table, qp.tablesFromBD)
+		result := response{"response": response{idCol: lastID}}
 
 		writeResponse(w, http.StatusOK, result, nil)
 	}
@@ -507,12 +570,12 @@ func (qp *queryParametrs) listPage(h *handler) http.HandlerFunc {
 		if qp.queryRequestParametrs.table != nil {
 			var err error
 			var status int
+
 			result, err, status = selectData(qp, h.DB)
 			if err != nil {
 				writeResponse(w, status, result, err)
 				return
 			}
-
 		} else {
 			var tables []string
 
@@ -539,8 +602,7 @@ func (h *handler) handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if qp == nil {
-		err := fmt.Errorf("queryParametrs is nil")
-		writeResponse(w, http.StatusInternalServerError, nil, err)
+		writeResponse(w, http.StatusInternalServerError, nil, fmt.Errorf("queryParametrs is nil"))
 		return
 	}
 
@@ -548,7 +610,7 @@ func (h *handler) handler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "DELETE":
-		handlerPage = qp.listPage(h)
+		handlerPage = qp.deletePage(h)
 	case "GET":
 		handlerPage = qp.listPage(h)
 	case "POST":
